15b37060db74f76244dce8d051d008a9f57c597b
Johannes Lohman <36767435+johanneslo1@users.noreply.github.com>
2021-07-18 16:02:25 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,24 +4,14 @@ set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 
 echo "Starts"
-#SOURCE_DIRECTORY="$1"
-DESTINATION_GITHUB_USERNAME="$2"
-DESTINATION_REPOSITORY_NAME="$3"
+FILES_TO_PUSH="$1"
+SOURCE_BRANCH="$3"
 USER_EMAIL="$4"
 USER_NAME="$5"
-DESTINATION_REPOSITORY_USERNAME="$6"
+REPOSITORY="$6"
 TARGET_BRANCH="$7"
 COMMIT_MESSAGE="$8"
 
-if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
-then
-  DESTINATION_REPOSITORY_USERNAME="$DESTINATION_GITHUB_USERNAME"
-fi
-
-if [ -z "$USER_NAME" ]
-then
-  USER_NAME="$DESTINATION_GITHUB_USERNAME"
-fi
 
 CLONE_DIR=$(mktemp -d)
 
@@ -29,7 +19,7 @@ echo "Cloning destination git repository"
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
-git clone --single-branch --branch "$TARGET_BRANCH" "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
+git clone --single-branch --branch "$TARGET_BRANCH" "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$REPOSITORY.git" "$CLONE_DIR"
 ls -la "$CLONE_DIR"
 
 TARGET_DIR=$(mktemp -d)
@@ -37,20 +27,6 @@ TARGET_DIR=$(mktemp -d)
 # but not anymore. Otherwise we had to remove the files from "$CLONE_DIR",
 # including "." and with the exception of ".git/"
 mv "$CLONE_DIR/.git" "$TARGET_DIR"
-
-#if [ ! -d "$SOURCE_DIRECTORY" ]
-#then
-	#echo "ERROR: $SOURCE_DIRECTORY does not exist"
-	#echo "This directory needs to exist when push-to-another-repository is executed"
-	#echo
-	#echo "In the example it is created by ./build.sh: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L19"
-	#echo
-	#echo "If you want to copy a directory that exist in the source repository"
-	#echo "to the target repository: you need to clone the source repository"
-	#echo "in a previous step in the same build section. For example using"
-	#echo "actions/checkout@v2. See: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L16"
-	#exit 1
-#fi
 
 echo "Copy contents to target git repository"
 cp -ra . "$TARGET_DIR"
@@ -64,8 +40,7 @@ COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
 
 echo "git add:"
-git add --force public/dist/.
-git add --force public/mix-manifest.json
+git add --force "$FILES_TO_PUSH"
 
 echo "git status:"
 git status
@@ -76,4 +51,4 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
 echo "git push origin:"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
-git push -f origin "refs/heads/main:$TARGET_BRANCH"
+git push -f origin "refs/heads/$SOURCE_BRANCH:$TARGET_BRANCH"
