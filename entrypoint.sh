924cd15d3b5db1f42f1be923161de2a15c99ee1b
Johannes Lohman <36767435+johanneslo1@users.noreply.github.com>
2021-07-18 11:52:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -71,7 +71,7 @@ git status
 
 echo "git diff-index:"
 # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
-git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
+git commit --message "$COMMIT_MESSAGE"
 
 echo "git push origin:"
 echo "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git - $TARGET_BRANCH"
